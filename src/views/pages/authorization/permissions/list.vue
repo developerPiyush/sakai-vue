2add70d101e9673f82874d582aa93693e673f1f4
piyush-crescentek <piyush.crescentek@outlook.com>
2023-12-06 23:31:46 +0530

    more changes updated

--- src/views/pages/authorization/permissions/list.vue
+++ src/views/pages/authorization/permissions/list.vue
@@ -1,3 +1,32 @@
 <template>
-    <h1>Permission</h1>
+    <div class="card">
+        <h3>Permissions</h3>
+        <DataTable :value="permissions" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]"
+            tableStyle="min-width: 50rem">
+            <Column field="name" header="Name" style="width: 25%"></Column>
+            <Column field="date" header="Date" style="width: 25%"></Column>
+        </DataTable>
+    </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const permissions = ref([
+    {
+        id: 1000,
+        name: 'can-create-customer',       
+        date: '2015-09-13',
+    },
+    {
+        id: 1000,
+        name: 'can-edit-customer',       
+        date: '2015-09-13',
+    },
+    {
+        id: 1000,
+        name: 'can-delete-customer',       
+        date: '2015-09-13',
+    },
+]);
+</script>
--- src/views/pages/authorization/roles/list.vue
+++ src/views/pages/authorization/roles/list.vue
@@ -1,3 +1,32 @@
 <template>
-    <h1>Roles</h1>
+    <div class="card">
+        <h3>Roles</h3>
+        <DataTable :value="roles" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]"
+            tableStyle="min-width: 50rem">
+            <Column field="name" header="Name" style="width: 25%"></Column>
+            <Column field="date" header="Date" style="width: 25%"></Column>
+        </DataTable>
+    </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const roles = ref([
+    {
+        id: 1000,
+        name: 'Admin',       
+        date: '2015-09-13',
+    },
+    {
+        id: 1000,
+        name: 'User',       
+        date: '2015-09-13',
+    },
+    {
+        id: 1000,
+        name: 'Agent',       
+        date: '2015-09-13',
+    },
+]);
+</script>
